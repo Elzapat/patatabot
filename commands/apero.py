ef4e073d39d65e80c0afa69aa8728cf66991d3ed
Anonymous <anonymous@anonymous.invalid>
2021-11-29 11:48:47 +0100

    commande apéro

---
+++ commands/apero.py
@@ -0,0 +1,14 @@
+from config import bot
+from discord import File
+
+@bot.command()
+async def apero(ctx):
+    """
+    Appel à tous de se rendre immédiatement chez Samuel pour un Apéro
+    """
+    await ctx.channel.send(
+        """
+        **APÉRO CHEZ SAMUEL MAINTENANT**
+        """,
+        file = File("images/ricard.gif")
+    )
