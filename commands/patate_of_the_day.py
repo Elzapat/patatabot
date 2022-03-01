2b0fbe5ee4458e6922939a73cdcb79b024b028b3
Anonymous <anonymous@anonymous.invalid>
2022-03-01 17:01:30 +0100

    ajout fonction requête et phrase-message à envoyer

--- commands/patate_of_the_day.py
+++ commands/patate_of_the_day.py
@@ -2,14 +2,17 @@ import json
 import requests
 import random
 
-from config import bot, PIXABAY_API_KEY
+from config import bot, PIXABAY_API_KEY , FETEDUJOUR_API_KEY
 from discord.ext import tasks
 
 current_patate_of_the_day: str = "https://perdu.com"
+current_fete: str="Aujoud'hui, c'est la saint pa-personne-tate"
 
 @bot.command(name="patate_of_the_day", aliases=["patateOfTheDay", "potd", "patateoftheday", "patate-of-the-day"])
 async def patate_of_the_day(ctx):
     await ctx.channel.send(current_patate_of_the_day)
+    await ctx.channel.send(current_fete)
+
 
 @tasks.loop(hours=24)
 async def new_patate_of_the_day_task():
@@ -42,4 +45,29 @@ async def new_patate_of_the_day_task():
     global current_patate_of_the_day
     current_patate_of_the_day = response["hits"][random_patate_idx]["largeImageURL"]
 
+# récupère la fête du jour en france
+@tasks.loop(hours=24)
+async def new_fete_of_the_day_task():
+    # requête
+    FETEDUJOUR_API = "https://fetedujour.fr/api/v2"
+    response = requests.get(f"{FETEDUJOUR_API}/{FETEDUJOUR_API_KEY}/json-saints")
+    # la clé de l'API peut être obtenue ici : https://fetedujour.fr/api/#apiKey
+    # exemple : IsosCILKo85ftKxL (générée avec la co de l'ISEN)
+    
+    if response.status_code != requests.codes.ok:
+        print(f"Error fetching ze fêtes du jour with status code {response.status_code}: ")
+        return    
+
+    response = response.json()
+    # on récupère la liste des noms 
+    current_fete = "Aujourd'hui nous fêtons "
+    for saint in response["saints"] :
+        if saint["gender"] == "F" :
+            current_fete += f"la sainte {saint['name']}, "
+        else:
+            current_fete += f"le saint {saint['name']}, "
+    current_fete = current_fete[:-2]+". "
+
+
 new_patate_of_the_day_task.start()
+new_fete_of_the_day_task()
