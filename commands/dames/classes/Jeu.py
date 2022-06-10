1b9e5bf3a64882805e8fa81dc5179b5cb2ea1d1b
Anonymous <anonymous@anonymous.invalid>
2022-06-10 14:54:15 +0000

    adapt to bot file structure

--- commands/dames/classes/Jeu.py
+++ commands/dames/classes/Jeu.py
@@ -37,7 +37,7 @@ class Jeu:
         self.__joueur1 = "joueur 1"
         self.__joueur2 = "joueur 2"
 
-        self.__savesDir = "dames_files/sauvegardes"
+        self.__savesDir = "assets/dames_sauvegardes"
 
         self.__strDep1 = "Pion *à déplacer* de **joueur X**  :\n*exit pour arrêter la partie*"
         self.__strDep2 = "case(s) de *destination* de **joueur X** :\n*exit pour arrêter la partie*"
